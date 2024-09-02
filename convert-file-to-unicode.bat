2eeeeab2a144daadd6b7eadf5066496a1f625883
oyogames2023 <oyogames2023@gmail.com>
2024-09-02 09:47:25 +0800

    1.转换编码 2.第三方库集成

--- convert-file-to-unicode.bat
+++ convert-file-to-unicode.bat
@@ -1,29 +1,11 @@
 @echo off
-setlocal enabledelayedexpansion
+SETLOCAL ENABLEDELAYEDEXPANSION
 
-REM 定义文件扩展名
-set "extensions=h cpp hpp json"
+SET Extensions=-Include *.h, *.cpp
 
-REM 遍历所有子目录
-for /r %%f in (*.*) do (
-    REM 获取文件扩展名
-    set "ext=%%~xf"
-    set "ext=!ext:~1!"
+SET PowerShellCommand=powershell -NoProfile -Command
 
-    REM 检查是否在我们定义的扩展名列表中
-    for %%e in (%extensions%) do (
-        if /i "!ext!"=="%%e" (
-            REM 输出当前处理的文件
-            echo Converting %%f to Unicode...
-            
-            REM 使用 powershell 转换编码为 UTF-8 (Unicode)
-            powershell -Command "Get-Content -Path '%%f' | Set-Content -Path '%%f.tmp' -Encoding Unicode"
-            
-            REM 替换原始文件
-            move /Y "%%f.tmp" "%%f"
-        )
-    )
-)
+%PowerShellCommand% "Get-ChildItem -Path '.\Include', '.\Src' -Recurse -File %Extensions% | ForEach-Object { $content = Get-Content -Path $_.FullName; Set-Content -Path $_.FullName -Value $content -Encoding UTF8 }"
 
-echo All files have been converted to Unicode encoding.
-pause
+echo Convert done.
+ENDLOCAL
