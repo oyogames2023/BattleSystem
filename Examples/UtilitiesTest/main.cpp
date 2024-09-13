b23712eed526cbb186ca796daa3269233e6b40ec
liukai <565434471@qq.com>
2024-09-13 20:08:22 +0800

    1.拆分字符串

--- Examples/UtilitiesTest/main.cpp
+++ Examples/UtilitiesTest/main.cpp
@@ -1,6 +1,35 @@
 #include "StringUtils.h"
 #include "FilesystemUtils.h"
 
+template <typename T>
+void PrintVector(const std::vector<T>& container)
+{
+	for (const auto& element : container)
+	{
+		std::cout << element << " ";
+	}
+	std::cout << std::endl;
+}
+
+template <typename Key, typename Value>
+void PrintMap(const std::map<Key, Value>& m)
+{
+	for (const auto& [k, v] : m)
+	{
+		std::cout << "key = " << k << ", " << "value = " << v << std::endl;
+	}
+}
+
+template <typename T>
+void PrintSet(const std::set<T>& container)
+{
+	for (const auto& element : container)
+	{
+		std::cout << element << " ";
+	}
+	std::cout << std::endl;
+}
+
 int main(int argc, char* argv[])
 {
 	namespace fs = std::filesystem;
@@ -12,6 +41,21 @@ int main(int argc, char* argv[])
 	std::cout << logDir.string() << std::endl;
 
 	std::cout << zeus::FilesystemUtils::GetExecutableDirectory() << std::endl;
+
+	auto v1 = zeus::StringUtils::SplitToSequence<std::string>("1,2,3");
+	auto v2 = zeus::StringUtils::SplitToSequence<uint64_t>("1,2,3");
+	PrintVector(v1);
+	PrintVector(v2);
+
+	auto s1 = zeus::StringUtils::SplitToSet<std::string>("1,2,3");
+	auto s2 = zeus::StringUtils::SplitToSet<std::string>("1,2,3");
+	PrintSet(s1);
+	PrintSet(s2);
+
+	auto m1 = zeus::StringUtils::SplitToMap<std::string, std::string>("1001,2|1002,1");
+	auto m2 = zeus::StringUtils::SplitToMap<uint32_t, uint64_t>("5001,2|5002,1");
+	PrintMap(m1);
+	PrintMap(m2);
 
 	return 0;
 }
--- Include/Common/Utilities/StringUtils.h
+++ Include/Common/Utilities/StringUtils.h
@@ -1,15 +1,152 @@
 #pragma once
 
 #include "Commom.h"
+#include "BattleConfig.h"
 
 #include "spdlog/spdlog.h"
 
 namespace zeus
 {
 
-	class  StringUtils
+	class BATTLE_API StringUtils
 	{
 	public:
+
+		template <typename T, template <typename, typename = std::allocator<T>> class Container = std::vector>
+		static Container<T> SplitToSequence(const std::string& str, const std::string& delimiter = ",")
+		{
+			Container<T> result;
+			size_t start = 0;
+			size_t end = str.find(delimiter);
+			while (end != std::string::npos)
+			{
+				std::istringstream iss(str.substr(start, end - start));
+				T value;
+				iss >> value;
+				if (!iss.fail())
+				{
+					result.insert(result.end(), value);
+				}
+				start = end + delimiter.length();
+				end = str.find(delimiter, start);
+			}
+			std::istringstream iss(str.substr(start));
+			T value;
+			iss >> value;
+			if (!iss.fail())
+			{
+				result.insert(result.end(), value);
+			}
+			return result;
+		}
+
+		template <template <typename, typename = std::allocator<std::string>> class Container = std::vector>
+		static Container<std::string> SplitToSequence(const std::string& str, const std::string& delimiter)
+		{
+			std::vector<std::string> result;
+			size_t start = 0;
+			size_t end = str.find(delimiter);
+			while (end != std::string::npos)
+			{
+				result.insert(result.end(), str.substr(start, end - start));
+				start = end + delimiter.length();
+				end = str.find(delimiter, start);
+			}
+			const std::string lastToken = str.substr(start);
+			if (!lastToken.empty())
+			{
+				result.insert(result.end(), lastToken);
+			}
+			return result;
+		}
+
+		template <typename T>
+		static std::set<T> SplitToSet(const std::string& str, const std::string& delimiter = ",")
+		{
+			std::set<T> result;
+			size_t start = 0;
+			size_t end = str.find(delimiter);
+			while (end != std::string::npos)
+			{
+				std::istringstream iss(str.substr(start, end - start));
+				T value;
+				iss >> value;
+				if (!iss.fail())
+				{
+					result.insert(value);
+				}
+				start = end + delimiter.length();
+				end = str.find(delimiter, start);
+			}
+			std::istringstream iss(str.substr(start));
+			T value;
+			iss >> value;
+			if (!iss.fail())
+			{
+				result.insert(value);
+			}
+			return result;
+		}
+
+		template <>
+		static std::set<std::string> SplitToSet(const std::string& str, const std::string& delimiter)
+		{
+			std::set<std::string> result;
+			size_t start = 0;
+			size_t end = str.find(delimiter);
+			while (end != std::string::npos)
+			{
+				result.insert(str.substr(start, end - start));
+				start = end + delimiter.length();
+				end = str.find(delimiter, start);
+			}
+			const std::string lastToken = str.substr(start, end - start);
+			if (!lastToken.empty())
+			{
+				result.insert(lastToken);
+			}
+			return result;
+		}
+
+		template <typename Key, typename Value>
+		static std::map<Key, Value> SplitToMap(const std::string& str, const std::string& delimiter = "|,")
+		{
+			std::map<Key, Value> result;
+			if (str.empty())
+			{
+				return result;
+			}
+			if (2 != delimiter.size())
+			{
+				return result;
+			}
+			auto splitItems = SplitToSequence<std::string>(str, std::string(1, delimiter[0]));
+			for (auto item : splitItems)
+			{
+				auto splitKeyValue = SplitToSequence<std::string>(item, std::string(1, delimiter[1]));
+				if (2 != splitKeyValue.size())
+				{
+					continue;
+				}
+				std::stringstream convertorKey(splitKeyValue[0]);
+				Key key;
+				convertorKey >> key;
+				if (convertorKey.fail())
+				{
+					continue;
+				}
+				std::stringstream convertorValue(splitKeyValue[1]);
+				Value value;
+				convertorValue >> value;
+				if (convertorValue.fail())
+				{
+					continue;
+				}
+				result[key] = value;
+			}
+
+			return result;
+		}
 		
 		template <typename... Args>
 		static std::string Format(const std::string& formatter, Args&&... args)
